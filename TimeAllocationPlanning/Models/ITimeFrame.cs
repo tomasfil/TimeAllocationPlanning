a4bb61a0e852173d03fd8422c073ee59dd6accad
Tomáš Filip <tomas.filip@meac.cz>
2021-08-25 12:15:56 +0200

    TimeFrame Model

---
+++ TimeAllocationPlanning/Models/ITimeFrame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TimeAllocationPlanning.Models
+{
+    public interface ITimeFrame
+    {
+        DateTime Date { get; }
+        DateTime From { get; }
+        DateTime To { get; }
+        int TotalMinutes { get; }
+
+        bool CollidesWith(TimeFrame timeAllocation);
+        void SetFrom(int hours, int minutes);
+        void SetTo(int hours, int minutes);
+    }
+}
---
+++ TimeAllocationPlanning/Models/TimeFrame.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeAllocationPlanning.Models
+{
+    public class TimeFrame : ITimeFrame
+    {
+        public TimeFrame(DateTime date)
+        {
+            Date = date;
+        }
+
+        public DateTime Date { get; private set; }
+        public DateTime From { get; private set; }
+
+        public DateTime To { get; private set; }
+
+        public int TotalMinutes => (To.Hour - From.Hour) * 60 + (To.Minute - From.Minute);
+
+        public bool CollidesWith(TimeFrame timeAllocation)
+        {
+            return
+                (timeAllocation.From >= From && timeAllocation.From < To) ||
+                (timeAllocation.To > From && timeAllocation.To <= To);
+        }
+
+        public void SetFrom(int hours, int minutes)
+        {
+            int hoursToSet, minutesToSet;
+            NormalizeTimeValues(hours, minutes, out hoursToSet, out minutesToSet);
+            From = new DateTime(Date.Year, Date.Month, Date.Day, hoursToSet, minutesToSet, 0);
+        }
+
+        public void SetTo(int hours, int minutes)
+        {
+            int hoursToSet, minutesToSet;
+            NormalizeTimeValues(hours, minutes, out hoursToSet, out minutesToSet);
+
+            To = new DateTime(Date.Year, Date.Month, Date.Day, hoursToSet, minutesToSet, 0);
+        }
+
+        private static void NormalizeTimeValues(int hours, int minutes, out int hoursToSet, out int minutesToSet)
+        {
+            hoursToSet = hours;
+            minutesToSet = minutes;
+            while (minutesToSet >= 60)
+            {
+                minutesToSet -= 60;
+                hoursToSet++;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Date:d}   {From:t} -> {To:t}";
+        }
+    }
+}
